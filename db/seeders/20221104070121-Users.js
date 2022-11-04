c25c4408ca00ecb1a535ecc1d634aabd85dd1203
Anna <tima2396@yandex.ru>
2022-11-04 10:10:06 +0300

    seedUser

---
+++ db/seeders/20221104070121-Users.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    const users = [
+      { login: 'User', password: '123123123' },
+    ];
+
+    const data = users.map((user) => ({
+      ...user,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }));
+
+    await queryInterface.bulkInsert('Users', data);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Users');
+  },
+};
