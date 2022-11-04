b5e7ba0652539b328e35a3cf5f3f9d99890bd7cd
Anna <tima2396@yandex.ru>
2022-11-04 13:27:34 +0300

    logInput2

--- routes/logRoute.js
+++ routes/logRoute.js
@@ -1,18 +1,21 @@
 const route = require('express').Router();
 const LoginPage = require('../views/LoginPage.jsx');
-const User = require('../db/models/user');
+const { User } = require('../db/models');
 // const users = [{ id: 1, tem: 'sadasd' }]
 route.get('/', async (req, res) => {
   res.renderComponent(LoginPage, { title: 'Start Page' });
 });
+
 route.post('/', async (req, res) => {
   const { login, password } = req.body;
-  const usesFromDb = await User.findOne({ where: { login } });
-  if (usesFromDb) {
-    res.redirect('/');
-  } else {
-    res.redirect('/reg');
+  const userLoginDb = await User.findOne({ where: { login } });
+  // console.log(userLoginDb);
+  if (userLoginDb) {
+    if (userLoginDb.password === password) {
+      res.redirect('/');
+    } else {
+      console.log('Неверный пароль');
+    }
   }
 });
-
 module.exports = route;
