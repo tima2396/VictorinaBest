75d0c30e035c1a7ec37d0e76b0f5ac6c4b9663a4
Alexandra <lubnina.alexandra@gmail.com>
2022-11-04 14:40:31 +0300

    clear db

--- db/models/theme.js
+++ db/models/theme.js
@@ -4,14 +4,9 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class Theme extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate({ Total, Question }) {
-      Theme.Total = Theme.hasMany(Total, { foreignKey: 'idTheme' });
-      Theme.Question = Theme.hasMany(Question, { foreignKey: 'idTheme' });
+      Theme.Totals = Theme.hasMany(Total, { foreignKey: 'idTheme' });
+      Theme.Questions = Theme.hasMany(Question, { foreignKey: 'idTheme' });
     }
   }
   Theme.init({
--- db/models/user.js
+++ db/models/user.js
@@ -4,13 +4,8 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate({ Total }) {
-      User.Total = User.hasMany(Total, { foreignKey: 'userId' });
+      User.Totals = User.hasMany(Total, { foreignKey: 'userId' });
     }
   }
   User.init({
